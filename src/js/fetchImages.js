f7d8172520cc823e6f3517541730c328a67d60a7
dan005005 <108746923+dan005005@users.noreply.github.com>
2023-01-24 23:32:57 +0200

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -4,6 +4,6 @@ export const fetchImages = async (value, page) => {
   const respons = await axios.get(
     `https://pixabay.com/api/?key=31800059-86399197816311d7a5cbd5e2b&q=${value}&image_type=photo&pretty=true&orientation=horizontal&page=${page}&per_page=21`
   );
-  const respValue = await respons;
+  //const respValue = await respons;
   return respValue.data;
 };
